1765dc26cfb17466e9d0b308c5871a916a756bdb
Enzo Viana <69684975+enzoviana@users.noreply.github.com>
2022-03-24 11:53:51 +0100

    Add files via upload

---
+++ shutdown.py
@@ -0,0 +1,39 @@
+import pyttsx3
+import speech_recognition as sr
+import os
+
+def take_commands():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listening")
+        r.pause_threshold= 0.7
+        audio = r.listen(source)
+
+        try:
+            print("Recognizing")
+            Query = r.recognize_google(audio)
+            print("its printing", Query,"")
+        except Exception as e :
+            print(e)
+            print("Repete")
+            return"NONE"
+    import time
+    time.sleep(2)
+    return Query            
+
+def Speak(audio):
+    engine = pyttsx3.init()
+    engine.say(audio)
+    engine.runAndWait()
+
+Speak("Do you want to shutdown your computer")
+while True:
+    command = take_commands()
+    if "no" in command:
+        Speak("Thank u sir I will not shutdown your computer")
+        break
+    if "yes" in command:
+        Speak("shutting the computer")
+        os.system("shutdown /s /t 30")
+        break
+    Speak ("Say that again sir")
